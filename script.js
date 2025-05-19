3a135fc1f7024ebffbb150fe66fdac0f86c37000
Jonier Pachon <jhonier.pachon1015@gmail.com>
2025-05-19 18:11:27 -0500

    Adding new slide feature to portfolio for improved content presentation and user interaction.

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ window.onscroll = () => {
    menu.classList.remove("bx-x");
    navbar.classList.remove("active");
 };
+
+// PORTFOLIO SWIPER
+
+let swiper = new Swiper(".portfolio-content", {
+   cssMode: true,
+   loop: true,
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   mousewheel: true,
+   keyboard: true,
+});
+
+// mousewheel: true, delete
+// keyboard: true, delete
