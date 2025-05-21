77243522a0ced3f7c745ded40dd44fec07a463ac
Jonier Pachon <jhonier.pachon1015@gmail.com>
2025-05-21 18:40:20 -0500

    Adding animations and enhancing the responsive design of my portfolio.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// NAVBAR
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -10,6 +11,22 @@ window.onscroll = () => {
    menu.classList.remove("bx-x");
    navbar.classList.remove("active");
 };
+
+// ABOUT SECTION
+
+// Typing Text Code
+const typed = new Typed(".multiple-text", {
+   strings: [
+      "HTML5 and CSS3.",
+      "Vanilla JavaScript.",
+      "Responsive Design.",
+      "Git and GitHub.",
+   ],
+   typeSpeed: 70,
+   backSpeed: 70,
+   backDelay: 1000,
+   loop: true,
+});
 
 // PORTFOLIO SWIPER
 
@@ -30,3 +47,26 @@ let swiper = new Swiper(".portfolio-content", {
 
 // mousewheel: true, delete
 // keyboard: true, delete
+
+// CONTACT SECTION
+
+document.addEventListener("DOMContentLoaded", function () {
+   document
+      .querySelector(".contact-form")
+      .addEventListener("submit", function (e) {
+         e.preventDefault(); // Prevent the default form submission
+
+         emailjs.sendForm("service_ummdl5h", "template_mrjirbf", this).then(
+            function () {
+               alert("✅ Message Sent Successfully!");
+               document.querySelector(".contact-form").reset(); // Reset the form
+            },
+            function (error) {
+               alert(
+                  "❌ Failed to send message. Please try again. \nError: " +
+                     error.text
+               );
+            }
+         );
+      });
+});
